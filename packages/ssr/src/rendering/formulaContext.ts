65363cd5bac85ea240114a76b0b1eab82cdd6c39
Erik Beuschau <bager.erik@gmail.com>
2024-12-10 09:23:54 +0100

    Move core formulas from global to FormulaContext

--- packages/ssr/src/rendering/formulaContext.ts
+++ packages/ssr/src/rendering/formulaContext.ts
@@ -4,9 +4,16 @@ import {
   FormulaContext,
   ToddleServerEnv,
 } from '@toddledev/core/dist/formula/formula'
+import {
+  isToddleFormula,
+  PluginFormula,
+} from '@toddledev/core/dist/formula/formulaTypes'
+import { FormulaHandler } from '@toddledev/core/dist/types'
 import { mapValues } from '@toddledev/core/dist/utils/collections'
 import { isDefined } from '@toddledev/core/dist/utils/util'
+import * as libFormulas from '@toddledev/std-lib/dist/formulas'
 import { getPathSegments } from '../routing/routing'
+import { ProjectFiles } from '../ssr.types'
 import { getRequestCookies } from './cookies'
 import { escapeSearchParameters } from './request'
 
@@ -19,15 +26,21 @@ export const getPageFormulaContext = ({
   component,
   req,
   logErrors,
+  files,
 }: {
   branchName: string
   component: PageComponent
   req: Request
   logErrors: boolean
+  files: ProjectFiles
 }): FormulaContext => {
   const env = serverEnv({ req, branchName, logErrors })
   const { searchParamsWithDefaults, hash, combinedParams, url } = getParameters(
     { component, req },
+  )
+  const coreFormulas = new Map<string, FormulaHandler>()
+  Object.entries(libFormulas).forEach(([name, module]) =>
+    coreFormulas.set('@toddle/' + name, module.default as any),
   )
   const formulaContext: FormulaContext = {
     data: {
@@ -52,6 +65,22 @@ export const getPageFormulaContext = ({
     root: null,
     package: undefined,
     env,
+    toddle: {
+      getFormula: (name: string) => coreFormulas.get(name),
+      getCustomFormula: (name: string, packageName: string | undefined) => {
+        let formula: PluginFormula<string> | undefined
+
+        if (isDefined(packageName)) {
+          formula = files.packages?.[packageName]?.formulas?.[name]
+        } else {
+          formula = files.formulas?.[name]
+        }
+
+        if (formula && isToddleFormula(formula)) {
+          return formula
+        }
+      },
+    },
   }
   formulaContext.data.Variables = mapValues(
     component.variables,
