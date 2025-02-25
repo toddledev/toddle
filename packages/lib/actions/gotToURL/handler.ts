cf5624500e6bcf0583027323cef51166487726eb
Mikkel <mikkel@toddle.dev>
2025-02-25 09:35:46 +0100

    fix: add open in new tab support to go to url

--- packages/lib/actions/gotToURL/handler.ts
+++ packages/lib/actions/gotToURL/handler.ts
@@ -1,12 +1,16 @@
 import type { ActionHandler } from '@toddledev/core/dist/types'
 
-const handler: ActionHandler = ([url], ctx) => {
+const handler: ActionHandler = ([url, openInNewTab], ctx) => {
   if (typeof url === 'string') {
     if (ctx.env.runtime === 'preview') {
       // Attempt to notify the parent about the failed navigation attempt
       window.parent?.postMessage({ type: 'blockedNavigation', url }, '*')
     } else {
-      window.location.href = url
+      if (openInNewTab) {
+        window.open(url, '_blank')
+      } else {
+        window.location.href = url
+      }
     }
   }
 }
