50d6a931c58772e6c5dc868a3fcd74ea625b868f
Erik Beuschau <bager.erik@gmail.com>
2025-03-11 10:22:04 +0100

    Adjust sync scripts in both examples

--- examples/cloudflare-worker/scripts/syncStaticAssets.js
+++ examples/cloudflare-worker/scripts/syncStaticAssets.js
@@ -2,6 +2,8 @@
 // This script is executed by the build process
 const fs = require('fs')
 import { RESET_STYLES } from '@toddledev/core/dist/styling/theme.const'
+
+// assets/_static/ folder
 fs.mkdirSync(`${__dirname}/../assets/_static`, { recursive: true })
 ;[
   'esm-page.main.js',
@@ -14,6 +16,9 @@ fs.mkdirSync(`${__dirname}/../assets/_static`, { recursive: true })
   ),
 )
 fs.writeFileSync(`${__dirname}/../assets/_static/reset.css`, RESET_STYLES)
+
+// dist/ folder
+fs.mkdirSync(`${__dirname}/../dist`, { recursive: true })
 fs.copyFileSync(
   `${__dirname}/../__project__/project.json`,
   `${__dirname}/../dist/project.json`,
--- examples/hono/scripts/syncStaticAssets.js
+++ examples/hono/scripts/syncStaticAssets.js
@@ -1,8 +1,10 @@
 // Copy files from the static-assets directory to the dist directory using fs
 // This script is executed by the build process
 const fs = require('fs')
-const path = require('path')
 import { RESET_STYLES } from '@toddledev/core/dist/styling/theme.const'
+
+// assets/_static/ folder
+fs.mkdirSync(`${__dirname}/../assets/_static`, { recursive: true })
 ;[
   'esm-page.main.js',
   'esm-page.main.js.map',
