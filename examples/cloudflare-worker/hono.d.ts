b5e0f626049c24c69719dc65283808b3ca2f9f78
Erik Beuschau <32451595+erik-beus@users.noreply.github.com>
2025-03-11 15:26:49 +0100

    Remove Cloudflare example (#174)
    See https://github.com/toddledev/cloudflare-example instead

--- examples/cloudflare-worker/hono.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ProjectFiles, ToddleProject } from '@toddledev/ssr/dist/ssr.types'
-
-export interface HonoEnv {
-  Variables: {
-    project: { files: ProjectFiles; project: ToddleProject }
-  }
-}
