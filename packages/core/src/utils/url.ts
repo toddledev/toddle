87c61f66f0e81c4cc50b6db15a3470a1cf25b2ad
Christian Kedde Esbensen <christian@toddle.dev>
2025-03-11 15:26:21 +0100

    Fix issue where arrays in search query wasnt proxied properly (#173)
    * Fix issue where arrays in search query wasnt proxied properly
    
    * Bump version
    
    * Prettier
    
    * Added test

--- packages/core/src/utils/url.ts
+++ packages/core/src/utils/url.ts
@@ -15,8 +15,12 @@ export const validateUrl = (url?: string | null, base?: string) => {
     const urlObject = new URL(url, base)
     // Creating a new URL object will not correctly encode the search params
     // So we need to iterate over them to make sure they are encoded as that happens when setting them explicitly
-    urlObject.searchParams.forEach((value, key) => {
-      urlObject.searchParams.set(key, value)
+    const searchCopy = new URLSearchParams(urlObject.searchParams)
+    searchCopy.forEach((value, key) => {
+      urlObject.searchParams.delete(key, value)
+    })
+    searchCopy.forEach((value, key) => {
+      urlObject.searchParams.append(key, value)
     })
     return urlObject
   } catch {
