df1c4bece4bd7037edb3077f1bf2e705232b8201
Lucas G <91744602+Xeiik@users.noreply.github.com>
2025-03-12 05:58:00 -0400

    Added logarithm formula (#177)
    * Added logarithm formula
    
    * Added checks for type and negative values
    
    * Added fixes from god_of_code
    
    Because he is the god of code.
    
    * Bump version to 0.0.5-alpha.3

---
+++ packages/lib/formulas/logarithm/handler.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, test } from '@jest/globals'
+import handler from './handler'
+
+describe('Formula: Logarithm', () => {
+  test('should return the logarithm of the number', () => {
+    expect(handler([null], undefined as any)).toBe(null)
+    expect(handler([], undefined as any)).toBe(null)
+    expect(handler(['hello'], undefined as any)).toBe(null)
+    expect(handler([-1], undefined as any)).toBe(null)
+    expect(handler([0], undefined as any)).toBe(-Infinity)
+    expect(handler([1], undefined as any)).toBe(0)
+    expect(handler([10], undefined as any)).toBe(2.302585092994046)
+  })
+})
---
+++ packages/lib/formulas/logarithm/handler.ts
@@ -0,0 +1,16 @@
+import type { FormulaHandler } from '@toddledev/core/dist/types'
+
+const handler: FormulaHandler<number> = ([a]) => {
+  // Check if input is not a number or is null
+  if (typeof a !== 'number' || Number.isNaN(a)) {
+    return null
+  }
+  // Number must be greater than or equal to 0
+  if (a < 0) {
+    return null
+  }
+
+  return Math.log(a)
+}
+
+export default handler
