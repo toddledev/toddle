e1e8adf68f84f745857be620805348b6d890ad33
Lucas G <91744602+Xeiik@users.noreply.github.com>
2025-03-12 04:24:20 -0400

    Added checks for type and negative values

--- packages/lib/formulas/logarithm/handler.test.ts
+++ packages/lib/formulas/logarithm/handler.test.ts
@@ -3,7 +3,7 @@ import handler from './handler'
 
 describe('Formula: Logarithm', () => {
   test('should return the logarithm of the number', () => {
-    expect(handler([-1], undefined as any)).toBe(NaN)
+    expect(handler([-1], undefined as any)).toBe(null)
     expect(handler([0], undefined as any)).toBe(-Infinity)
     expect(handler([1], undefined as any)).toBe(0)
     expect(handler([10], undefined as any)).toBe(2.302585092994046)
