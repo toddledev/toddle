60075c361826a6d77e0e83138c31b47a8281d308
Lucas G <91744602+Xeiik@users.noreply.github.com>
2025-03-12 05:02:26 -0400

    Added fixes from god_of_code
    Because he is the god of code.

--- packages/lib/formulas/logarithm/handler.test.ts
+++ packages/lib/formulas/logarithm/handler.test.ts
@@ -3,6 +3,9 @@ import handler from './handler'
 
 describe('Formula: Logarithm', () => {
   test('should return the logarithm of the number', () => {
+    expect(handler([null], undefined as any)).toBe(null)
+    expect(handler([], undefined as any)).toBe(null)
+    expect(handler(['hello'], undefined as any)).toBe(null)
     expect(handler([-1], undefined as any)).toBe(null)
     expect(handler([0], undefined as any)).toBe(-Infinity)
     expect(handler([1], undefined as any)).toBe(0)
