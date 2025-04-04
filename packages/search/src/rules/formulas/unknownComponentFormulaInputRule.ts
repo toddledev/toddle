052ebf099f5b7c5ac5d5ecdb8849c138ca7a6b1d
Erik Beuschau <bager.erik@gmail.com>
2025-04-04 12:41:03 +0200

    Also exclude index path formulas

--- packages/search/src/rules/formulas/unknownComponentFormulaInputRule.ts
+++ packages/search/src/rules/formulas/unknownComponentFormulaInputRule.ts
@@ -14,7 +14,7 @@ export const unknownComponentFormulaInputRule: Rule<{
       nodeType !== 'formula' ||
       value.type !== 'path' ||
       value.path?.[0] !== 'Args' ||
-      ['@toddle.parent', 'item'].includes(value.path[1]) ||
+      ['@toddle.parent', 'item', 'index'].includes(value.path[1]) ||
       value.path.length < 2 ||
       path[0] !== 'components' ||
       path[2] !== 'formulas' ||
