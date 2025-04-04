da4dabb66ebad9905427ba731b377d98065358c3
Erik Beuschau <32451595+erik-beus@users.noreply.github.com>
2025-04-04 08:36:38 +0200

    Add rules for unknown formula inputs (#218)
    * Add unknown component formula input rule
    
    * Also add unknownProjectFormulaInputRule
    
    * Remove console.log
    
    * Bump version

--- packages/search/src/problems.worker.ts
+++ packages/search/src/problems.worker.ts
@@ -29,7 +29,9 @@ import { unknownTriggerEventRule } from './rules/events/unknownTriggerEventRule'
 import { legacyFormulaRule } from './rules/formulas/legacyFormulaRule'
 import { noReferenceComponentFormulaRule } from './rules/formulas/noReferenceComponentFormulaRule'
 import { noReferenceProjectFormulaRule } from './rules/formulas/noReferenceProjectFormulaRule'
+import { unknownComponentFormulaInputRule } from './rules/formulas/unknownComponentFormulaInputRule'
 import { unknownFormulaRule } from './rules/formulas/unknownFormulaRule'
+import { unknownProjectFormulaInputRule } from './rules/formulas/unknownProjectFormulaInputRule'
 import { unknownProjectFormulaRule } from './rules/formulas/unknownProjectFormulaRule'
 import { unknownRepeatItemFormulaRule } from './rules/formulas/unknownRepeatItemFormulaRule'
 import { noUnnecessaryConditionFalsy } from './rules/logic/noUnnecessaryConditionFalsy'
@@ -139,6 +141,7 @@ const RULES = [
   unknownApiInputRule,
   unknownAttributeRule,
   unknownClassnameRule,
+  unknownComponentFormulaInputRule,
   unknownComponentRule,
   unknownComponentSlotRule,
   unknownContextFormulaRule,
@@ -150,6 +153,7 @@ const RULES = [
   unknownEventRule,
   unknownFormulaRule,
   unknownProjectActionRule,
+  unknownProjectFormulaInputRule,
   unknownProjectFormulaRule,
   unknownRepeatItemFormulaRule,
   unknownSetUrlParameterRule,
---
+++ packages/search/src/rules/formulas/unknownComponentFormulaInputRule.test.ts
@@ -0,0 +1,93 @@
+import { searchProject } from '../../searchProject'
+import { unknownComponentFormulaInputRule } from './unknownComponentFormulaInputRule'
+
+describe('unknownComponentFormulaInput', () => {
+  test('should detect invalid component formula input references', () => {
+    const problems = Array.from(
+      searchProject({
+        files: {
+          components: {
+            test: {
+              name: 'test',
+              nodes: {},
+              formulas: {
+                myFormula: {
+                  name: 'Test',
+                  formula: {
+                    type: 'function',
+                    name: '@toddle/concatenate',
+                    arguments: [
+                      {
+                        name: '0',
+                        formula: { type: 'path', path: ['Args', 'Unknown'] },
+                      },
+                      {
+                        name: '1',
+                        formula: { type: 'path', path: ['Args', null as any] },
+                      },
+                    ],
+                    variableArguments: true,
+                    display_name: 'Concatenate',
+                  },
+                  arguments: [{ name: 'Input', testValue: 'Input' }],
+                  memoize: false,
+                  exposeInContext: false,
+                },
+              },
+              apis: {},
+              attributes: {},
+              variables: {},
+            },
+          },
+        },
+        rules: [unknownComponentFormulaInputRule],
+      }),
+    )
+
+    expect(problems).toHaveLength(2)
+    expect(problems[0].code).toBe('unknown component formula input')
+    expect(problems[0].details).toEqual({ name: 'Unknown' })
+    expect(problems[1].code).toBe('unknown component formula input')
+    expect(problems[1].details).toEqual({ name: null })
+  })
+  test('should not detect valid component formula input references', () => {
+    const problems = Array.from(
+      searchProject({
+        files: {
+          components: {
+            test: {
+              name: 'test',
+              nodes: {},
+              formulas: {
+                myFormula: {
+                  name: 'Test',
+                  formula: {
+                    type: 'function',
+                    name: '@toddle/concatenate',
+                    arguments: [
+                      {
+                        name: '0',
+                        formula: { type: 'path', path: ['Args', 'Input'] },
+                      },
+                    ],
+                    variableArguments: true,
+                    display_name: 'Concatenate',
+                  },
+                  arguments: [{ name: 'Input', testValue: 'Input' }],
+                  memoize: false,
+                  exposeInContext: false,
+                },
+              },
+              apis: {},
+              attributes: {},
+              variables: {},
+            },
+          },
+        },
+        rules: [unknownComponentFormulaInputRule],
+      }),
+    )
+
+    expect(problems).toHaveLength(0)
+  })
+})
